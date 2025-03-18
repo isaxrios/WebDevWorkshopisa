e4bc7dfd23daaaca4e6e54e62698319969cb85a2
p3n2 <peneloperiosdiaz@gmail.com>
2025-03-18 17:57:56 -0400

    commit

--- main.py
+++ main.py
@@ -18,19 +18,31 @@ def get_tasks():
 @app.get("/task/(task_id)")
 def get_task_by_id(task_id: int):
     found = True
+
+@app.get("/get-task/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+@app.put("/{task_id}")
+def update_task(task_id : int, updated: Task):
+    for task in tasks:
+        if task.id == task_id:
+            task.description = updated.description
+            task.is_complete = updated.is_complete
+            return "Updated Task"
+    return "Task Not Found"    
     
-#@app.get("/")
-#def get_task():
- #   return "Hello World"
+@app.post("/task")
+def create_task(task : Task):
+   tasks.append(task)
+   return "Task Added"
 
-#@app.put("/")
-#def get_task():
- #   return "Hello World"
-
-#@app.post("/")
-#def create_task():
- #   return "Hello World"
-
-#@app.delete("/")
-#def create_task():
-    #return "Hello World"
+@app.delete("/task//{task_id}")
+def delete_task(task_id : int):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(index)
+            return "Deleted Task"
+    return "Task Not Found"
